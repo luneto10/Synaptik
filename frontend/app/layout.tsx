9c7c8bc14617416bcf0fd54979cfaee697218912
Paulo Rogerio <paulo21102110@gmail.com>
2026-04-16 17:35:37 -0300

    fix: hydration

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
             className={`${inter.variable} ${jetbrainsMono.variable} h-full antialiased`}
             suppressHydrationWarning
         >
-            <body className="min-h-full flex flex-col">
+            <body className="min-h-full flex flex-col" suppressHydrationWarning>
                 <Providers>{children}</Providers>
             </body>
         </html>
